93683718a8331b1d57867f135e4005534a9ea730
Nico 67 <nicofaerber2010@gmail.com>
2025-10-22 16:21:26 +0200

    40_If_Bedingung

---
+++ 40_If_Bedingung_Basics/variablen4.js
@@ -0,0 +1,15 @@
+let a = 0
+
+
+// a > 10 größer
+// a < 10 kleiner
+// a == 10 gleich
+if (a > 0) {
+    console.log("The number is positive")
+}
+if (a < 0) {
+    console.log("The number is negative")
+}
+if (a == 0) {
+    console.log("The number is Zero")
+}

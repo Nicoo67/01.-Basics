f1e647352e4d8ea8986f0d12ecc895279e08cc81
Nico 67 <nicofaerber2010@gmail.com>
2025-10-22 16:21:42 +0200

    40_If_Beding + 41_If_Bed

---
+++ 40_If_Bedingung_Basics/Aufgabe2.js
@@ -0,0 +1,11 @@
+let trafficLight = "red"
+
+if (trafficLight == "red") {
+    console.log("Stop")
+}
+if (trafficLight == "yellow") {
+    console.log("Coution")
+}
+if (trafficLight == "green") {
+    console.log("You may go")
+}
---
+++ 41_If_Aufgaben_Else/Aufgabe1.js
@@ -0,0 +1,11 @@
+let a = 1
+
+
+// a > 10 größer
+// a < 10 kleiner
+// a = 10 gleich
+if (a >= 10) {
+    console.log("Die Zahl ist Groß");
+} else 
+    console.log("Die Zahl ist klein");
+    

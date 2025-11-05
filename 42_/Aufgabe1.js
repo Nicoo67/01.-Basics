93763541250e9503bc082ec650faa2ff57a67d04
Nico 67 <nicofaerber2010@gmail.com>
2025-11-05 16:11:51 +0100

    42_Aufgabe1

---
+++ 42_/Aufgabe1.js
@@ -0,0 +1,13 @@
+let randomNumber = Math.floor(Math.random() * 10);
+
+if (5 > randomNumber) {
+    console.log("The Number is less than 5")
+
+} else if (5 < randomNumber) {
+    console.log("The Number is more than 5")
+} else {
+    console.log("The Number is 5")
+}
+
+
+

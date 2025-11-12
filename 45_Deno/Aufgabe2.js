d784a777759c310e94e0b709a11057cd202fff6c
Nico 67 <nicofaerber2010@gmail.com>
2025-11-12 15:50:38 +0100

    Aufgabe 2 Fertig

---
+++ 45_Deno/Aufgabe2.js
@@ -0,0 +1,20 @@
+let number = 0
+
+//number > 10 positiv
+//number < 10 negativ
+//number = 10 gleich
+
+if (number > 0) {
+    console.log("The number is positive");
+    
+} else ("The number is not positive");
+
+if (number < 0) {
+    console.log("The number is negative");
+    
+} else ("The number is positive or 0");
+
+if (number == 0){
+    console.log("The number is zero");
+
+} else ("The number is not 0");

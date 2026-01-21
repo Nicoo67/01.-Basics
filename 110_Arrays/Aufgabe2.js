c1944441984aca3eedc6ecd86e74d8f34d4c09e2
Nico 67 <nicofaerber2010@gmail.com>
2026-01-21 15:40:03 +0100

    Aufgabe 2 /70

---
+++ 110_Arrays/Aufgabe2.js
@@ -0,0 +1,10 @@
+let arr = [10, 25, 423, 4333];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+let average = sum / arr.length;
+
+console.log("The average is" + " " + average); 
